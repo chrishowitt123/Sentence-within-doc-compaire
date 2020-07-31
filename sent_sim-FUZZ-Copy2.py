ea0813f25105cf3cb53952dc410fbf314b4206a1
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2020-07-31 18:08:34 +0100

    Update sent_sim-FUZZ-Copy2.py

--- sent_sim-FUZZ-Copy2.py
+++ sent_sim-FUZZ-Copy2.py
@@ -1,23 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[12]:
-
-
 import docx2txt
 import itertools
 import pandas as pd
 from difflib import SequenceMatcher
 import re
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
+from rapidfuzz import fuzz
+from rapidfuzz import process
 from nltk.tokenize import PunktSentenceTokenizer
 
 text = docx2txt.process('MY COPY_Final HLA.P.1631_Adenda do EIA do Bloco 17_20200620 - Copy EN (1) - Copy.docx')
 sent_tokenizer = PunktSentenceTokenizer(text)
 sents = sent_tokenizer.tokenize(text)
-sents
-
+sents = set(sents)
 x_list = []
 y_list = []
 score = []
@@ -32,5 +25,5 @@ data_tuples = list(zip(x_list,y_list,score))
 
 results = pd.DataFrame(data_tuples, columns=['X','Y', 'Score'])  
 results = results.sort_values(by=['Score'], ascending=False)
-results[results['Score'] > 70].to_csv('results.csv')
+results[results['Score'] > 70].to_csv('resultsSet.csv')
 
